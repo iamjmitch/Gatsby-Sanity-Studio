4a395aadccbdf69889d7eeb4a74f0549561460bd
James Mitchell <iamjmitch1990@gmail.com>
2020-10-05 01:41:10 +1000

    siteSettings Schema Created

---
+++ schemas/siteSettings.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'siteSettings',
+  title: 'Site Settings',
+  type: 'document',
+  fields: [
+    // ... other fields ...
+    {
+      name: 'siteName',
+      title: 'Site Name',
+      type: 'string',
+    },
+    {
+      name: 'siteSubtitle',
+      title: 'Site Subtitle',
+      type: 'string',
+    },
+  ],
+};
